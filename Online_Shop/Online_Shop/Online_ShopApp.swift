bf7df3ac76c5e38bf5e2839c7e49436fd3d42271
Alexander <a.a.grigoryev@icloud.com>
2022-06-09 17:43:42 +0300

    Initial commit

---
+++ Online_Shop/Online_Shop/Online_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_ShopApp.swift
+//  Online_Shop
+//
+//  Created by Alexander Grigoryev on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Online_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
