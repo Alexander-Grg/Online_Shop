b6a44cc09dd32653d093f849db05eeb3e7499428
Alexander <a.a.grigoryev@icloud.com>
2022-06-30 11:20:09 +0300

    Logout tested

--- Online_Shop/Online_ShopTests/AuthTest.swift
+++ Online_Shop/Online_ShopTests/AuthTest.swift
@@ -12,22 +12,26 @@ import Alamofire
 
 class AuthTest: XCTestCase {
     var expectation = XCTestExpectation(description: "TestingAuth...")
+    var expectationOfLogout = XCTestExpectation(description: "TestingLogout...")
     var auth: AuthRequestFactory!
     var isAuthSuccesfull: Bool!
+    var isLogout: Bool!
     
     override func setUpWithError() throws {
         let container = ContainerBuilder().makeContainer()
         let factory = RequestFactory(container: container)
         auth = factory.makeAuthRequestFactory()
         isAuthSuccesfull = false
+        isLogout = false
     }
 
     override func tearDownWithError() throws {
         auth = nil
         isAuthSuccesfull = nil
+        isLogout = nil
     }
     
-    func testAuth() {
+    func testLogin() {
         auth.login(userName: userDataStub.username,
                    password: userDataStub.password) { [weak self] response in
             switch response.result {
@@ -39,6 +43,20 @@ class AuthTest: XCTestCase {
             self?.expectation.fulfill()
         }
         wait(for: [expectation], timeout: 10.0)
-        XCTAssertTrue(isAuthSuccesfull, "The auth is unsuccessful")
+        XCTAssertTrue(isAuthSuccesfull, "Auth is unsuccessful")
+    }
+    
+    func testLogout() {
+        auth.logout(id: userDataStub.id) { [weak self] response in
+            switch response.result {
+            case .success(let data):
+                self?.isLogout = data.result == 1 ? true : false
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            self?.expectationOfLogout.fulfill()
+        }
+        wait(for: [expectationOfLogout], timeout: 10.0)
+        XCTAssertTrue(isLogout, "Logout is unsuccessful")
     }
 }
